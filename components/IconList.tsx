9ae69afb2a7871ac04a838d2a3bc8225bd52bf09
fernandobosco <fbosco@dlocal.com>
2022-01-02 19:13:01 -0300

    update linkedin

--- components/IconList.tsx
+++ components/IconList.tsx
@@ -12,7 +12,7 @@ import { IconLink } from "./icon-link";
 export function IconList() {
   return (<div className="mt-4 list-disc flex flex-row space-x-2">
     <IconLink href="mailto:fernando.bbosco@gmail.com" Icon={HiOutlineMail} />
-    <IconLink href="" Icon={FaLinkedin} />
+    <IconLink href="https://www.linkedin.com/in/fernandobelotto/" Icon={FaLinkedin} />
     <IconLink href="https://github.com/fernandobelotto" Icon={FaGithub} />
     <IconLink href="https://wa.me/5519997773727" Icon={FaWhatsapp} />
     <IconLink href="https://open.spotify.com/user/12160766820?si=989c664a687d42cf" Icon={FaSpotify} />
