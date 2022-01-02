847193be71817fa7dfc5366de110ed4b05787c7e
fernandobosco <fbosco@dlocal.com>
2022-01-02 14:07:20 -0300

    container added

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Index(props: NextPage) {
     <>
       <div className={`${mode ? "" : "dark"}`}>
         <div className="bg-slate-50 dark:bg-gray-900">
-          <div className="p-10">
+          <div className="p-10 container mx-auto">
             <Head>
               <title>Fernando Belotto</title>
               <meta name="description" content="Fernando Belotto Website" />
@@ -54,26 +54,6 @@ export default function Index(props: NextPage) {
                 in São Paulo, Brazil 🇧🇷
               </h1>
               <IconList />
-              <hr className="mt-3" />
-              <h1 className="text-3xl mt-10 dark:text-white">Projects</h1>
-              <p className='dark:text-white'>Open source projects that you can checkout</p>
-              <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-4 gap-5">
-                <ProjectCardList />
-              </div>
-              <h1 className="text-3xl mt-10 dark:text-white">CRUD Examples</h1>
-              <p className='dark:text-white'>Examples projects per technologies implementing crud operations</p>
-              <div className="mt-10 grid base:grid-cols-1 md:grid-cols-5 gap-5">
-                <CrudCardList />
-              </div>
-              <h1 className="text-3xl mt-10 dark:text-white">Awesomes Websites</h1>
-              <p className='dark:text-white'>Checkout some of the websites that i created base on 'awesome' repos using docusaurus</p>
-              <div className="mt-10 grid base:grid-cols-1 md:grid-cols-5 gap-5">
-                <AwesomeCardList />
-              </div>
-              <h1 className="text-3xl mt-10 dark:text-white">Blog Posts</h1>
-              <div className="mt-10 grid sm:grid-cols-2 md:grid-cols-3 gap-5">
-                <BlogCardList />
-              </div>
               <h1 className="text-3xl mt-10 dark:text-white">About</h1>
               <p
                 className="
@@ -97,6 +77,27 @@ export default function Index(props: NextPage) {
                 loves teaching so he uses his spare time to create a video
                 tutorials or writing blog posts about his new discoveries
               </p>
+              <hr className="mt-3" />
+              <h1 className="text-3xl mt-10 dark:text-white">Projects</h1>
+              <p className='dark:text-white'>Open source projects that you can checkout</p>
+              <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-4 gap-5">
+                <ProjectCardList />
+              </div>
+              <h1 className="text-3xl mt-10 dark:text-white">CRUD Examples</h1>
+              <p className='dark:text-white'>Examples projects per technologies implementing crud operations</p>
+              <div className="mt-10 grid base:grid-cols-1 md:grid-cols-5 gap-5">
+                <CrudCardList />
+              </div>
+              <h1 className="text-3xl mt-10 dark:text-white">Awesomes Websites</h1>
+              <p className='dark:text-white'>Checkout some of the websites that i created base on 'awesome' repos using docusaurus</p>
+              <div className="mt-10 grid base:grid-cols-1 md:grid-cols-5 gap-5">
+                <AwesomeCardList />
+              </div>
+              <h1 className="text-3xl mt-10 dark:text-white">Blog Posts</h1>
+              <div className="mt-10 grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+                <BlogCardList />
+              </div>
+
               <h1 className="text-3xl mt-10 dark:text-white">Technologies</h1>
               <TechList></TechList>
             </main>
